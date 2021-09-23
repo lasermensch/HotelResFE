e5770ac550f64538c80634db88d38f64bc7416eb
Erik Sundberg <sten.erik.sundberg@gmail.com>
2021-09-23 13:38:47 +0200

    Add project files.

---
+++ HotelResFE/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using HotelReservationFE.Views;
+using Prism.Ioc;
+using Prism.Regions;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HotelReservationFE
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    /// 
+
+public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            var w = Container.Resolve<MainWindow>();
+            return w;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            var regionManager = Container.Resolve<IRegionManager>();
+            var contentRegion = regionManager.Regions["ContentRegion"];
+            var homeView = Container.Resolve<Login>();
+            
+        }
+    }
+}
